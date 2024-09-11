191daa604f74dbb8079cbe36c635d9cdd4a6abc3
1104232532@qq.com <1104232532@qq.com>
2024-09-11 21:03:37 +0800

    [v1.0版本]简单的计算功能：整数连续加减乘，删除一位，清空功能

---
+++ widget.cpp
@@ -0,0 +1,145 @@
+#include "widget.h"
+#include "ui_widget.h"
+#include <QString>
+#include <QChar>
+#include <QDebug>
+#include <QPushButton>
+
+int calculate_result(int first_num,int second_num, QChar sym){
+    if(sym == '+'){
+        return first_num+second_num;
+    }else if(sym == '-'){
+        return first_num-second_num;
+    }else if(sym == '*'){
+        return first_num * second_num;
+    }else{
+        return 0;
+    }
+}
+
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    this->setWindowTitle("计算器");
+    //初始化为calStr
+    calStr = "";
+    //QString resultStr;
+    sym = '0';
+    //ui->label_calculate->setText(calStr);
+    //ui->label_calculate->clear();
+
+    //开始计算
+    first_num = 0;
+    second_num = 0;
+
+    //点击数字键 返回数字
+    connect(ui->num_0,&QPushButton::clicked,this,[this](){
+        //qDebug() << "点击0";
+        calStr += "0";
+        ui->label_calculate->setText(calStr);
+    });
+    connect(ui->num_1,&QPushButton::clicked,this,[this](){
+        calStr += "1";
+        ui->label_calculate->setText(calStr);
+    });
+    connect(ui->num_2,&QPushButton::clicked,this,[this](){
+        calStr += "2";
+        ui->label_calculate->setText(calStr);
+    });
+    connect(ui->num_3,&QPushButton::clicked,this,[this](){
+        calStr += "3";
+        ui->label_calculate->setText(calStr);
+    });
+    connect(ui->num_4,&QPushButton::clicked,this,[this](){
+        calStr += "4";
+        ui->label_calculate->setText(calStr);
+    });
+    connect(ui->num_5,&QPushButton::clicked,this,[this](){
+        calStr += "5";
+        ui->label_calculate->setText(calStr);
+    });
+    connect(ui->num_6,&QPushButton::clicked,this,[this](){
+        calStr += "6";
+        ui->label_calculate->setText(calStr);
+    });
+    connect(ui->num_7,&QPushButton::clicked,this,[this](){
+        calStr += "7";
+        ui->label_calculate->setText(calStr);
+    });
+    connect(ui->num_8,&QPushButton::clicked,this,[this](){
+        calStr += "8";
+        ui->label_calculate->setText(calStr);
+    });
+    connect(ui->num_9,&QPushButton::clicked,this,[this](){
+        calStr += "9";
+        ui->label_calculate->setText(calStr);
+    });
+
+    //点击符号键 返回符号
+    connect(ui->symEqual,&QPushButton::clicked,this,[this](){
+        //如果只输入了一位
+        if(sym == '0'){
+            ui->label_calculate->setText(calStr);
+            return;
+        }
+        //将符号后面的保存为第二位
+        int symPos = calStr.lastIndexOf(QChar(sym));
+        first_num = calStr.left(symPos).toInt();
+        second_num = calStr.mid(symPos+1).toInt();
+
+        //连算，结果保存为第一个数
+        first_num = calculate_result(first_num,second_num,sym);
+        second_num = 0;
+        //计算得出结果
+        resultStr = QString::number(first_num);
+
+        calStr = resultStr;
+        //返回结果
+        ui->label_calculate->setText(resultStr);
+    });
+
+    connect(ui->symPlus,&QPushButton::clicked,this,[this](){
+        first_num = calStr.toInt();
+        calStr += "+";
+        sym = '+';
+        ui->label_calculate->setText(calStr);
+    });
+    connect(ui->symMinus,&QPushButton::clicked,this,[this](){
+        first_num = calStr.toInt();
+        calStr += "-";
+        sym = '-';
+        ui->label_calculate->setText(calStr);
+    });
+    connect(ui->symMul,&QPushButton::clicked,this,[this](){
+        first_num = calStr.toInt();
+        calStr += "*";
+        sym = '*';
+        ui->label_calculate->setText(calStr);
+    });
+
+    //点击功能 返回功能
+    connect(ui->symDel,&QPushButton::clicked,this,[&](){
+        //删除一位
+        calStr.chop(1);
+        ui->label_calculate->setText(calStr);
+
+    });
+    connect(ui->symClear,&QPushButton::clicked,this,[&](){
+        //清除计算
+        calStr = "";
+        first_num = 0;
+        second_num = 0;
+        sym = '0';
+        ui->label_calculate->clear();
+    });
+
+
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
---
+++ widget.h
@@ -0,0 +1,31 @@
+#pragma once //防止头文件重复包含
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class Widget;
+}
+QT_END_NAMESPACE
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = nullptr);
+
+
+    ~Widget();
+    QString calStr;
+    QString resultStr;
+    QChar sym;
+    int first_num;
+    int second_num;
+
+private:
+    Ui::Widget *ui;
+};
+#endif // WIDGET_H
